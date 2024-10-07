9a505ca7925e09d5d4acd155885da637a0039e8d
fsuropaty <fauzy.ramadhany@gmail.com>
2024-10-07 13:48:18 +0900

    add copy_static function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
-from textnode import TextNode
+import os
+import shutil
+
+
+def copy_static(source, target):
+    if not os.path.exists(target):
+        print("make directory :", target)
+        os.mkdir(target)
+
+    for file in os.listdir(source):
+        spath = os.path.join(source, file)
+        tpath = os.path.join(target, file)
+
+        if os.path.isfile(spath):
+            print(f"copying file from {spath} to {tpath}")
+            shutil.copy(spath, tpath)
+        else:
+            copy_static(spath, tpath)
+
 
 def main():
-    tnode = TextNode("This is a text node", "bold", "https://boot.dev")
-    print(tnode)
+
+    source = "static"
+    target = "public"
+
+    if os.path.exists(target):
+        shutil.rmtree(target)
+
+    copy_static(source, target)
+
 
 main()
-
-
